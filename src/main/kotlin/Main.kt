b0ae9f73c5f5681d0c6b197339dc0d17eb0d7b53
Lukasz Kalnik <lukasz.kalnik@gmail.com>
2023-05-02 20:39:02 +0200

    Solve day 1 part 1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,13 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import okio.FileSystem
+import okio.Path.Companion.toPath
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun main(args: Array<String>) {
+    val path = "input.txt".toPath()
+    val inventoriesString = FileSystem.SYSTEM.read(path) { readUtf8() }
+    val maxInventory = inventoriesString.dropLast(1).split("\n\n")
+        .map { inventoryString -> inventoryString.split("\n") }
+        .map { inventory -> inventory.map { item -> item.toInt() } }
+        .map { inventory -> inventory.sum() }
+        .max()
+    println(maxInventory)
 }
